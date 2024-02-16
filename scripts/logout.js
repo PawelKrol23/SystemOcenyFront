50dab16033ea0c10503c1370e6454a1eea766e09
Squezer123 <krzysztof.kordiukiewicz@gmail.com>
2024-02-16 11:30:20 +0100

    Deleted temporary front

--- scripts/logout.js
+++
@@ -1,6 +0,0 @@
-function logout(button){
-    button.addEventListener("click",() =>{
-        cookies.clearCookies();
-        window.location.href = `http://127.0.0.1:5500`;
-    })
-}
--- scripts/utils.js
+++
@@ -1,6 +0,0 @@
-var utils = { 
-    redirection(role){
-        if(role === "index") window.location.href = 'http://127.0.0.1:5500/index.html'
-        else window.location.href = `http://127.0.0.1:5500/${role}Page.html`;
-    }
-}
