10c5750a1a936380fdf412956c0071f6a6bc5b00
PawelKrol23 <p.krol18a@gmail.com>
2024-03-06 17:34:39 +0100

    Wystylowano header i dodano logo pb

--- src/Components/LoginHeader/LoginHeader.tsx
+++ src/Components/LoginHeader/LoginHeader.tsx
@@ -1,8 +1,17 @@
+import PbLogo from "../../Assets/PbLogo.png";
 
 export const LoginHeader = () => {
   return (
-    <header>
-    
+    <header style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "50px",
+      padding: "1rem",
+      backgroundColor: "#012404",
+      margin: "0",
+    }}>
+      <img src={PbLogo} alt={"pb logo"} />
     </header>
   )
 }
