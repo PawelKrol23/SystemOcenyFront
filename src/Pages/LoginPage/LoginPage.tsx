edf46ad1a0b9f24b59a4cead71ae9c298c37a99e
PawelKrol23 <p.krol18a@gmail.com>
2024-03-06 17:47:25 +0100

    Naprawiono style w widoku logowania

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -10,7 +10,7 @@ export function LoginPage() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          height: "100vh",
+          height: "calc(100vh - 100px)",
         }}
       >
         <LoginForm />
