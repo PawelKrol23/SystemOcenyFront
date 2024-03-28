c6169201a9ddae6a54439963bd6bbcf4b6e58cc3
PawelKrol23 <p.krol18a@gmail.com>
2024-03-29 00:32:36 +0100

    Dodano ikonke do przycisku

--- src/Components/AddButton/AddButton.tsx
+++ src/Components/AddButton/AddButton.tsx
@@ -1,8 +1,9 @@
 import { Button } from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 export const AddButton = () => {
   return (
-    <Button variant="contained" color="success">
+    <Button variant="contained" color="success" startIcon={<AddIcon />}>
       Dodaj
     </Button>
   )
