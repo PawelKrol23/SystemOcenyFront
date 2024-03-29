55a5b42f93d5d1565bbd8d226961b0c6c3342280
PawelKrol23 <p.krol18a@gmail.com>
2024-03-29 01:07:12 +0100

    Dodano modal do pagea

--- src/Components/AddButton/AddButton.tsx
+++ src/Components/AddButton/AddButton.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 
-export const AddButton = () => {
+export const AddButton = (onButtonClick: () => void) => {
   return (
-    <Button variant="contained" color="success" startIcon={<AddIcon />} style={{
+    <Button variant="contained" color="success" startIcon={<AddIcon />}
+            onClick={onButtonClick}
+            style={{
       width: "fit-content",
       margin: "2rem auto",
       fontWeight: "bold"
--- src/Pages/UserPages/UserAchivHisPage/UserAchivHisPage.tsx
+++ src/Pages/UserPages/UserAchivHisPage/UserAchivHisPage.tsx
@@ -1,8 +1,19 @@
 import { AddButton, UserAchivHisTable } from "../../../Components";
 import UserPageLayout from "../../../Layout/UserPageLayout/UserPageRootLayout";
 import { colors } from "../../../Theme/variables";
+import OsiagniecieFormModal from "../../../Components/OsiagniecieFormModal/OsiagniecieFormModal.tsx";
+import { useState } from "react";
 
 export function UserAchivHisPage() {
+  const [modalOpen, setModalOpen] = useState(false);
+  
+  const onModalClose = () => {
+    setModalOpen(false)
+  }
+  
+  const onAddButtonClick = () => {
+    setModalOpen(true)
+  }
 
   return (
     <UserPageLayout>
@@ -21,7 +32,8 @@ export function UserAchivHisPage() {
         }}>Historia Osiągnięć</p>
       </div>
       <UserAchivHisTable></UserAchivHisTable>
-      <AddButton/>
+      <AddButton onButtonClick={onAddButtonClick}/>
+      <OsiagniecieFormModal open={modalOpen} onClose={onModalClose} />
     </UserPageLayout>
   );
 }
