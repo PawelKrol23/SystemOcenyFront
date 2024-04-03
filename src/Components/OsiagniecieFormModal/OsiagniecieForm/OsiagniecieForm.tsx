734b503a1dec2fadb3af0bc0b2189f0aa94a0b4a
PawelKrol23 <p.krol18a@gmail.com>
2024-04-03 18:40:55 +0200

    Poprawiono wyglad i naprawiono zamykanie modalu

--- src/Components/OsiagniecieFormModal/OsiagniecieForm/OsiagniecieForm.tsx
+++ src/Components/OsiagniecieFormModal/OsiagniecieForm/OsiagniecieForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Grid, Paper } from '@mui/material';
+import { TextField, Button } from '@mui/material';
 
 export const OsiagniecieForm = () => {
   const [formData, setFormData] = useState({
@@ -24,54 +24,53 @@ export const OsiagniecieForm = () => {
   };
   
   return (
-    <Grid container justifyContent="center" alignItems="center" style={{ height: '100vh' }}>
-      <Grid item xs={10} sm={8} md={6} lg={4}>
-        <Paper elevation={3} style={{ padding: 20 }}>
-          <form onSubmit={handleSubmit}>
-            <TextField
-              fullWidth
-              label="First Name"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              margin="normal"
-              variant="outlined"
-            />
-            <TextField
-              fullWidth
-              label="Last Name"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              margin="normal"
-              variant="outlined"
-            />
-            <TextField
-              fullWidth
-              label="Email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              margin="normal"
-              variant="outlined"
-            />
-            <TextField
-              fullWidth
-              label="Message"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-              multiline
-              rows={4}
-              margin="normal"
-              variant="outlined"
-            />
-            <Button type="submit" variant="contained" color="primary" fullWidth>
-              Submit
-            </Button>
-          </form>
-        </Paper>
-      </Grid>
-    </Grid>
+    <form onSubmit={handleSubmit}>
+      <TextField
+        fullWidth
+        label="First Name"
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+        color={"info"}
+      />
+      <TextField
+        fullWidth
+        label="Last Name"
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+        color={"info"}
+      />
+      <TextField
+        fullWidth
+        label="Email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        margin="normal"
+        variant="outlined"
+        color={"info"}
+      />
+      <TextField
+        fullWidth
+        label="Message"
+        name="message"
+        value={formData.message}
+        onChange={handleChange}
+        multiline
+        rows={4}
+        margin="normal"
+        variant="outlined"
+        color={"info"}
+        style={{ marginBottom: 10 }}
+      />
+      <Button type="submit" variant="contained" color="success" fullWidth>
+        Submit
+      </Button>
+    </form>
   );
 };
--- src/Components/OsiagniecieFormModal/OsiagniecieFormModal.tsx
+++ src/Components/OsiagniecieFormModal/OsiagniecieFormModal.tsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { OsiagniecieForm } from "./OsiagniecieForm/OsiagniecieForm.tsx";
 
 const style = {
   position: 'absolute',
@@ -30,12 +30,7 @@ export const OsiagniecieFormModal = (props: OsiagniecieFormModalProps) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <OsiagniecieForm/>
         </Box>
       </Modal>
     </div>
