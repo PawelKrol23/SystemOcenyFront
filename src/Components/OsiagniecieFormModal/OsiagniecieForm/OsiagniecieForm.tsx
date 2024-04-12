5dc6b661ffdcf9c4e13398f33090e5ac9f9eb359
PawelKrol23 <p.krol18a@gmail.com>
2024-04-12 13:32:07 +0200

    Dodano walidacje do reszty formularza

--- src/Components/OsiagniecieFormModal/OsiagniecieForm/OsiagniecieForm.tsx
+++ src/Components/OsiagniecieFormModal/OsiagniecieForm/OsiagniecieForm.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { TextField, Button, InputLabel, FormControl, Select, SelectChangeEvent } from '@mui/material';
+import { TextField, Button, InputLabel, FormControl, Select, SelectChangeEvent, FormHelperText } from '@mui/material';
 import MenuItem from "@mui/material/MenuItem";
 import { useSOP } from "../../../Context/ContextProvider.tsx";
 import { Podkategoria } from "../../../Interfaces/UserType.tsx";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers";
+import { DatePicker, DateValidationError } from "@mui/x-date-pickers";
 import AddIcon from "@mui/icons-material/Add";
 import dayjs from "dayjs";
+import { FieldChangeHandlerContext } from "@mui/x-date-pickers/internals";
 
 export interface OsiagniecieFormProps {
   afterSubmit: () => void
@@ -28,6 +29,8 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
   });
   const { getUserAllPodkategorias, getUserSession } = useSOP();
   const [AllPodkategorias, setAllPodkategorias] = useState<Podkategoria[] | null>([]);
+  const [iloscPunktowHelperText, setIloscPunktowHelperText] = useState<string>('');
+  const [formInvalid, setFormInvalid] = useState<boolean>(false);
   
   const setPodkategorias = async () => {
     const podkategorias = await getUserAllPodkategorias();
@@ -38,11 +41,47 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
     setPodkategorias();
   }, []);
   
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+  React.useEffect(() => {
+    checkIloscPunktowValidity()
+  }, [formData])
+  
+  React.useEffect(() => {
+    setFormInvalid(errorForm.nazwa || errorForm.iloscPunktow || errorForm.data || errorForm.podkategoria);
+  }, [errorForm])
+  
+  const checkIloscPunktowValidity = () => {
+    if(formData.podkategoria === '') {
+      return;
+    }
+    
+    const podkategoria = AllPodkategorias?.find(el => {
+      return el.nazwa === formData.podkategoria
+    });
+    
+    if (!podkategoria) {
+      return;
+    }
+    
+    if(formData.iloscPunktow > podkategoria.maxPunktow || formData.iloscPunktow < podkategoria.minPunktow) {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        iloscPunktow: true,
+      }));
+      setIloscPunktowHelperText(`Ilość punktów powinna być z przedziału od ${podkategoria.minPunktow} do ${podkategoria.maxPunktow}`);
+    } else {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        iloscPunktow: false,
+      }));
+      setIloscPunktowHelperText("");
+    }
+  }
+  
+  const handleIloscPunktowChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value,
+      iloscPunktow: +value,
     }));
   };
   
@@ -67,6 +106,22 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
   
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if(formData.podkategoria === '') {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        podkategoria: true
+      }))
+      return
+    }
+    
+    if(formData.nazwa.length < 1 || formData.nazwa.length > 250) {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        nazwa: true
+      }))
+      return
+    }
+    
     const userSession = getUserSession();
     if(!userSession || !userSession.token) {
       alert(`User not logged in`);
@@ -114,14 +169,25 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
   };
   
   const handleSelectChange = (event: SelectChangeEvent) => {
+    const value = event.target.value
     setFormData((prevData) => ({
       ...prevData,
-      podkategoria: event.target.value,
+      podkategoria: value,
     }));
+    if(!value || value === '') {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        podkategoria: true,
+      }))
+    } else {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        podkategoria: false,
+      }))
+    }
   };
   
-  const handleDatePickerChange = (date: dayjs.Dayjs | null) => {
-    console.log(date)
+  const handleDatePickerChange = (date: dayjs.Dayjs | null, context: FieldChangeHandlerContext<DateValidationError>) => {
     if(date) {
       setFormData((prevState) => {
         return {
@@ -129,7 +195,18 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
           data: date.toDate()
         }
       })
-      console.log(formData.data)
+    }
+    
+    if(context.validationError) {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        data: true,
+      }))
+    } else {
+      setErrorForm((prevData) => ({
+        ...prevData,
+        data: false,
+      }))
     }
   }
   
@@ -151,18 +228,18 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
         fullWidth
         label="Ilość Punktów"
         name="iloscPunktow"
-        type={"number"}
         value={formData.iloscPunktow}
-        onChange={handleChange}
+        onChange={handleIloscPunktowChange}
         margin="normal"
         variant="outlined"
         color={"info"}
+        error={errorForm.iloscPunktow}
+        helperText={iloscPunktowHelperText}
       />
-      <FormControl fullWidth color={"info"} margin={"normal"}>
+      <FormControl fullWidth color={"info"} margin={"normal"} error={errorForm.podkategoria}>
         <InputLabel id="demo-simple-select-label" color={"info"}>Podkategoria</InputLabel>
         <Select
           color={"info"}
-          labelId="demo-simple-select-label"
           value={formData.podkategoria}
           label="Podkategoria"
           onChange={handleSelectChange}
@@ -171,6 +248,7 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
             <MenuItem key={podkategoria.idPodKategorii} value={podkategoria.nazwa}>{podkategoria.nazwa}</MenuItem>
           ))}
         </Select>
+        <FormHelperText>{errorForm.podkategoria ? "Podkategoria nie może być pusta" : ""}</FormHelperText>
       </FormControl>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <div style={{
@@ -184,7 +262,7 @@ export const OsiagniecieForm = (props: OsiagniecieFormProps) => {
           <DatePicker onChange={handleDatePickerChange} value={dayjs(formData.data)}/>
         </div>
       </LocalizationProvider>
-      <Button variant="contained" color="success" startIcon={<AddIcon />} type={"submit"} fullWidth
+      <Button variant="contained" color="success" startIcon={<AddIcon />} type={"submit"} fullWidth disabled={formInvalid}
        style={{
          margin: "0.5rem auto",
          fontWeight: "bold"
